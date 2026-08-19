84f3fe70d02591c4056340c377d5b7fc5ead47f7
unknown <sima.alliancemart@gmail.com>
2026-08-19 17:03:45 +0600

    positive-negative checker

---
+++ PN-checker/pn-checker.js
@@ -0,0 +1,11 @@
+function check(num) {
+  if (num > 0) {
+    console.log("Positive");
+  } else if (num < 0) {
+    console.log("Negative");
+  } else {
+    console.log("Zero");
+  }
+}
+
+check(-5);
